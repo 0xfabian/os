986f1ac54ab7ea66a1ae03ec289f28268e6b09b6
0xfabian <dumitru_fabian0@yahoo.com>
2025-02-19 23:29:10 +0200

    unused for now

--- include/ds/table.h
+++ include/ds/table.h
@@ -1,82 +1,82 @@
-#pragma once
+// #pragma once
 
-#include <mem/heap.h>
+// #include <mem/heap.h>
 
-template <typename T>
-struct TableEntry
-{
-    T value;
-    TableEntry* next;
-};
+// template <typename T>
+// struct TableEntry
+// {
+//     T value;
+//     TableEntry* next;
+// };
 
-template <typename T>
-struct Table
-{
-    struct Iterator
-    {
-        TableEntry<T>* entry;
+// template <typename T>
+// struct Table
+// {
+//     struct Iterator
+//     {
+//         TableEntry<T>* entry;
 
-        Iterator(TableEntry<T>* _entry) : entry(_entry) {}
+//         Iterator(TableEntry<T>* _entry) : entry(_entry) {}
 
-        T& operator*() { return entry->value; }
-        Iterator& operator++() { entry = entry->next; return *this; }
-        bool operator!=(const Iterator& other) { return entry != other.entry; }
-    };
+//         T& operator*() { return entry->value; }
+//         Iterator& operator++() { entry = entry->next; return *this; }
+//         bool operator!=(const Iterator& other) { return entry != other.entry; }
+//     };
 
-    TableEntry<T>* head = nullptr;
-    TableEntry<T>* tail = nullptr;
-    size_t _size = 0;
+//     TableEntry<T>* head = nullptr;
+//     TableEntry<T>* tail = nullptr;
+//     size_t _size = 0;
 
-    bool empty() { return _size == 0; };
-    size_t size() { return _size; };
+//     bool empty() { return _size == 0; };
+//     size_t size() { return _size; };
 
-    T* alloc()
-    {
-        TableEntry<T>* entry = new TableEntry<T>();
+//     T* alloc()
+//     {
+//         TableEntry<T>* entry = new TableEntry<T>();
 
-        if (!head)
-        {
-            head = entry;
-            tail = entry;
-        }
-        else
-        {
-            tail->next = entry;
-            tail = entry;
-        }
+//         if (!head)
+//         {
+//             head = entry;
+//             tail = entry;
+//         }
+//         else
+//         {
+//             tail->next = entry;
+//             tail = entry;
+//         }
 
-        _size++;
+//         _size++;
 
-        return &entry->value;
-    }
+//         return &entry->value;
+//     }
 
-    void free(T* ptr)
-    {
-        TableEntry<T>* prev = nullptr;
-        TableEntry<T>* entry = head;
+//     void free(T* ptr)
+//     {
+//         TableEntry<T>* prev = nullptr;
+//         TableEntry<T>* entry = head;
 
-        while (entry)
-        {
-            if (&entry->value == ptr)
-            {
-                if (prev)
-                    prev->next = entry->next;
-                else
-                    head = entry->next;
+//         while (entry)
+//         {
+//             if (&entry->value == ptr)
+//             {
+//                 if (prev)
+//                     prev->next = entry->next;
+//                 else
+//                     head = entry->next;
 
-                if (entry == tail)
-                    tail = prev;
+//                 if (entry == tail)
+//                     tail = prev;
 
-                _size--;
-                delete entry;
-                return;
-            }
+//                 _size--;
+//                 delete entry;
+//                 return;
+//             }
 
-            prev = entry;
-            entry = entry->next;
-        }
-    }
+//             prev = entry;
+//             entry = entry->next;
+//         }
+//     }
 
-    Iterator begin() { return Iterator(head); }
-    Iterator end() { return Iterator(nullptr); }
-};
+//     Iterator begin() { return Iterator(head); }
+//     Iterator end() { return Iterator(nullptr); }
+// };
