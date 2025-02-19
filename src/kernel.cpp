a1c19878c8d027d6f547f0c118b941293fdc99dc
0xfabian <dumitru_fabian0@yahoo.com>
2025-02-19 23:44:00 +0200

    clean

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -3,18 +3,6 @@
 #include <arch/gdt.h>
 #include <arch/idt.h>
 
-#include <fs/file.h>
-
-int fbterm_write(File* file, const char* buf, size_t size, size_t offset)
-{
-    fbterm.write(buf, size);
-    return size;
-}
-
-struct FileOps fbterm_ops = {
-    .write = fbterm_write,
-};
-
 extern "C" void kmain(void)
 {
     if (!LIMINE_BASE_REVISION_SUPPORTED)
@@ -31,19 +19,6 @@ extern "C" void kmain(void)
     idt.init();
 
     heap.init(2000);
-
-    File* file = File::open("/dev/fbterm", 0);
-
-    if (!file)
-        idle();
-
-    file->ops = fbterm_ops;
-
-    file_table.debug();
-
-    file->write("hello\n", 6);
-    file->write("world\n", 6);
-    file->close();
 
     idle();
 }
