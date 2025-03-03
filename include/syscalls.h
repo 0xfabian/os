f67eba992375995d1617401389367ba4bada37fa
0xfabian <dumitru_fabian0@yahoo.com>
2025-03-03 12:33:28 +0200

    temp impl

--- include/syscalls.h
+++ include/syscalls.h
@@ -6,7 +6,25 @@
 
 enum SyscallNumber
 {
-    SYS_DEBUG = 0
+    SYS_READ = 0,
+    SYS_WRITE,
+    SYS_OPEN,
+    SYS_CLOSE,
+    SYS_FORK = 57,
+    SYS_EXECVE = 59,
+    SYS_EXIT,
+    SYS_WAIT,
+
+    SYS_DEBUG = 512
 };
+
+isize sys_read(unsigned int fd, char* buf, usize size);
+isize sys_write(unsigned int fd, const char* buf, usize size);
+int sys_open(const char* path, u32 flags);
+int sys_close(unsigned int fd);
+int sys_fork();
+int sys_execve(const char* path, char* const argv[], char* const envp[]);
+void sys_exit(int status);
+int sys_wait(int pid, int* status, int options);
 
 void sys_debug();
--- src/syscalls.cpp
+++ src/syscalls.cpp
@@ -4,14 +4,78 @@ int do_syscall(CPU* cpu, int num)
 {
     switch (num)
     {
-    case SYS_DEBUG:         sys_debug();                return 0;
-    default:                                            return -ERR_NOT_IMPL;
+    case SYS_READ:          return sys_read(cpu->rdi, (char*)cpu->rsi, cpu->rdx);
+    case SYS_WRITE:         return sys_write(cpu->rdi, (const char*)cpu->rsi, cpu->rdx);
+    case SYS_OPEN:          return sys_open((const char*)cpu->rdi, cpu->rsi);
+    case SYS_CLOSE:         return sys_close(cpu->rdi);
+    case SYS_FORK:          return sys_fork();
+    case SYS_EXECVE:        return sys_execve((const char*)cpu->rdi, (char* const*)cpu->rsi, (char* const*)cpu->rdx);
+    case SYS_EXIT:          sys_exit(cpu->rdi); return 0;
+    case SYS_WAIT:          return sys_wait(cpu->rdi, (int*)cpu->rsi, cpu->rdx);
+    case SYS_DEBUG:         sys_debug(); return 0;
+    default:                return -ERR_NOT_IMPL;
     }
 }
 
 extern "C" void syscall_handler(CPU* cpu, int num)
 {
     cpu->rax = do_syscall(cpu, num);
+}
+
+isize sys_read(unsigned int fd, char* buf, usize size)
+{
+    ikprintf("sys_read(%d, %p, %lu)\n", fd, buf, size);
+
+    return -1;
+}
+
+isize sys_write(unsigned int fd, const char* buf, usize size)
+{
+    ikprintf("sys_write(%d, %p, %lu)\n", fd, buf, size);
+
+    return -1;
+}
+
+int sys_open(const char* path, u32 flags)
+{
+    ikprintf("sys_open(%s, %x)\n", path, flags);
+
+    return -1;
+}
+
+int sys_close(unsigned int fd)
+{
+    ikprintf("sys_close(%d)\n", fd);
+
+    return -1;
+}
+
+int sys_fork()
+{
+    ikprintf("sys_fork()\n");
+
+    return 0;
+}
+
+int sys_execve(const char* path, char* const argv[], char* const envp[])
+{
+    ikprintf("sys_execve(%s, %p, %p)\n", path, argv, envp);
+
+    return -1;
+}
+
+void sys_exit(int status)
+{
+    ikprintf("sys_exit(%d)\n", status);
+
+    idle();
+}
+
+int sys_wait(int pid, int* status, int options)
+{
+    ikprintf("sys_wait(%d, %p, %d)\n", pid, status, options);
+
+    return -1;
 }
 
 void sys_debug()
