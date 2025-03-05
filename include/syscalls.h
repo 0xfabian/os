7d6640c644be1b44fa361581ea1267dc57eb0d16
0xfabian <dumitru_fabian0@yahoo.com>
2025-03-05 23:05:56 +0200

    more syscalls

--- include/syscalls.h
+++ include/syscalls.h
@@ -10,10 +10,14 @@ enum SyscallNumber
     SYS_WRITE,
     SYS_OPEN,
     SYS_CLOSE,
+    SYS_IOCTL = 16,
+    SYS_DUP = 32,
+    SYS_DUP2,
     SYS_FORK = 57,
     SYS_EXECVE = 59,
     SYS_EXIT,
     SYS_WAIT,
+    SYS_GETDENTS = 78,
 
     SYS_DEBUG = 512
 };
@@ -22,9 +26,13 @@ isize sys_read(unsigned int fd, char* buf, usize size);
 isize sys_write(unsigned int fd, const char* buf, usize size);
 int sys_open(const char* path, u32 flags);
 int sys_close(unsigned int fd);
+int sys_ioctl(int fd, int cmd, void* arg);
+int sys_dup(int oldfd);
+int sys_dup2(int oldfd, int newfd);
 int sys_fork();
 int sys_execve(const char* path, char* const argv[], char* const envp[]);
 void sys_exit(int status);
 int sys_wait(int pid, int* status, int options);
+int sys_getdents(int fd, char* buf, usize size);
 
 void sys_debug();
--- src/syscalls.cpp
+++ src/syscalls.cpp
@@ -8,10 +8,14 @@ int do_syscall(CPU* cpu, int num)
     case SYS_WRITE:         return sys_write(cpu->rdi, (const char*)cpu->rsi, cpu->rdx);
     case SYS_OPEN:          return sys_open((const char*)cpu->rdi, cpu->rsi);
     case SYS_CLOSE:         return sys_close(cpu->rdi);
+    case SYS_IOCTL:         return sys_ioctl(cpu->rdi, cpu->rsi, (void*)cpu->rdx);
+    case SYS_DUP:           return sys_dup(cpu->rdi);
+    case SYS_DUP2:          return sys_dup2(cpu->rdi, cpu->rsi);
     case SYS_FORK:          return sys_fork();
     case SYS_EXECVE:        return sys_execve((const char*)cpu->rdi, (char* const*)cpu->rsi, (char* const*)cpu->rdx);
     case SYS_EXIT:          sys_exit(cpu->rdi); return 0;
     case SYS_WAIT:          return sys_wait(cpu->rdi, (int*)cpu->rsi, cpu->rdx);
+    case SYS_GETDENTS:      return sys_getdents(cpu->rdi, (char*)cpu->rsi, cpu->rdx);
     case SYS_DEBUG:         sys_debug(); return 0;
     default:                return -ERR_NOT_IMPL;
     }
@@ -24,78 +28,122 @@ extern "C" void syscall_handler(CPU* cpu, int num)
 
 isize sys_read(unsigned int fd, char* buf, usize size)
 {
-    // ikprintf("sys_read(%d, %p, %lu)\n", fd, buf, size);
+    auto file = running->fdt.get(fd);
 
-    isize ret = fbterm.read(buf, size);
+    if (!file)
+        return file.error();
 
-    // this could never return here
-
-    return ret;
+    return file->read(buf, size);
 }
 
 isize sys_write(unsigned int fd, const char* buf, usize size)
 {
-    // ikprintf("sys_write(%d, %p, %lu)\n", fd, buf, size);
+    auto file = running->fdt.get(fd);
 
-    // ikprintf("%lu: ", running->tid);
-    fbterm.write(buf, size);
+    if (!file)
+        return file.error();
 
-    return size;
+    return file->write(buf, size);
 }
 
 int sys_open(const char* path, u32 flags)
 {
-    ikprintf("sys_open(%s, %x)\n", path, flags);
+    FDTable* fdt = &running->fdt;
 
-    return -1;
+    int fd = fdt->get_unused();
+
+    if (fd < 0)
+        return fd;
+
+    auto file = File::open(path, flags);
+
+    if (!file)
+        return file.error();
+
+    fdt->install(fd, file.ptr);
+
+    return fd;
 }
 
 int sys_close(unsigned int fd)
 {
-    ikprintf("sys_close(%d)\n", fd);
+    FDTable* fdt = &running->fdt;
 
-    return -1;
+    auto file = fdt->uninstall(fd);
+
+    if (!file)
+        return file.error();
+
+    file->close();
+
+    return 0;
+}
+
+int sys_ioctl(int fd, int cmd, void* arg)
+{
+    auto file = running->fdt.get(fd);
+
+    if (!file)
+        return file.error();
+
+    return file->ioctl(cmd, arg);
+}
+
+int sys_dup(int oldfd)
+{
+    FDTable* fdt = &running->fdt;
+
+    int newfd = fdt->get_unused();
+
+    if (newfd < 0)
+        return newfd;
+
+    auto file = fdt->get(oldfd);
+
+    if (!file)
+        return file.error();
+
+    fdt->install(newfd, file->dup());
+
+    return newfd;
+}
+
+int sys_dup2(int oldfd, int newfd)
+{
+    FDTable* fdt = &running->fdt;
+
+    if (!fdt->is_usable(newfd))
+        return -ERR_BAD_FD;
+
+    auto file = fdt->get(oldfd);
+
+    if (!file)
+        return file.error();
+
+    fdt->install(newfd, file->dup());
+
+    return newfd;
 }
 
 int sys_fork()
 {
-    // ikprintf("sys_fork()\n");
-
     Task* task = running->fork();
 
-    if (task)
-    {
-        task->ready();
-        return task->tid;
-    }
+    if (!task)
+        return -1;
 
-    return -1;
+    task->ready();
+
+    return task->tid;
 }
 
 int sys_execve(const char* path, char* const argv[], char* const envp[])
 {
-    // ikprintf("sys_execve(%s, %p, %p)\n", path, argv, envp);
-
-    // ikprintf("argv = { ");
-
-    // int i = 0;
-    // while (argv[i])
-    // {
-//     ikprintf("\"%s\", ", argv[i]);
-    //     i++;
-    // }
-
-    // ikprintf("nullptr }\n");
-
     return running->execve(path, argv, envp);
-
-    // return -1;
 }
 
 void sys_exit(int status)
 {
-    // ikprintf("sys_exit(%d)\n", status);
-
     running->exit(status);
 
     ikprintf("Task %lu should be zombie, but it's not?\n", running->tid);
@@ -105,11 +153,19 @@ void sys_exit(int status)
 
 int sys_wait(int pid, int* status, int options)
 {
-    // ikprintf("sys_wait(%d, %p, %d)\n", pid, status, options);
-
     running->wait();
 
     return 0;
+}
+
+int sys_getdents(int fd, char* buf, usize size)
+{
+    auto file = running->fdt.get(fd);
+
+    if (!file)
+        return file.error();
+
+    return file->iterate(buf, size);
 }
 
 void sys_debug()
