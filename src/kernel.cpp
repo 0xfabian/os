78cedf867d947e17c298262b85be5c5c75d64d69
0xfabian <dumitru_fabian0@yahoo.com>
2025-03-13 17:11:12 +0200

    start with /mnt/sh

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -71,7 +71,7 @@ extern "C" void kmain(void)
     sched_init();
 
     // Task* kbd = Task::from(keyboard_task);
-    Task* sh = Task::from("/sh");
+    Task* sh = Task::from("/mnt/sh");
 
     // kbd->ready();
     sh->ready();
