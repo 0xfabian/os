0844fc2ff0fa3628c9d0c21d52b104db87891b27
0xfabian <dumitru_fabian0@yahoo.com>
2025-03-24 22:41:45 +0200

    +O_DIRECTORY

--- include/fs/file.h
+++ include/fs/file.h
@@ -4,13 +4,14 @@
 #include <string.h>
 #include <fs/inode.h>
 
-#define O_RDONLY    0
-#define O_WRONLY    1
-#define O_RDWR      2
-#define O_CREAT     0x40
-#define O_EXCL      0x80
-#define O_TRUNC     0x200
-#define O_APPEND    0x400
+#define O_RDONLY        0
+#define O_WRONLY        1
+#define O_RDWR          2
+#define O_CREAT         0x40
+#define O_EXCL          0x80
+#define O_TRUNC         0x200
+#define O_APPEND        0x400
+#define O_DIRECTORY     0x10000
 
 #define SEEK_SET 0
 #define SEEK_CUR 1
