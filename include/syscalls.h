97277ae1f9a99b9a2f71644504f3de2b2ae20a2c
0xfabian <dumitru_fabian0@yahoo.com>
2025-03-31 17:14:54 +0300

    many more syscalls for tcc, mostly dummy tho

--- include/syscalls.h
+++ include/syscalls.h
@@ -11,18 +11,37 @@ enum SyscallNumber
     SYS_OPEN,
     SYS_CLOSE,
     SYS_SEEK = 8,
+    SYS_MPROTECT = 10,
+    SYS_BRK = 12,
     SYS_IOCTL = 16,
+    SYS_WRITEV = 20,
     SYS_DUP = 32,
     SYS_DUP2,
     SYS_FORK = 57,
     SYS_EXECVE = 59,
     SYS_EXIT,
     SYS_WAIT,
+    SYS_UNAME = 63,
+    SYS_FCNTL = 72,
     SYS_GETDENTS = 78,
     SYS_GETCWD,
     SYS_CHDIR,
+    SYS_UNLINK = 87,
+    SYS_READLINK = 89,
+    SYS_GETUID = 102,
+    SYS_GETGID = 104,
+    SYS_GETEUID = 107,
+    SYS_GETEGID,
+    SYS_ARCH_PRCTL = 158,
+    SYS_OPENAT = 257,
 
     SYS_DEBUG = 512
+};
+
+struct iovec
+{
+    void* iov_base;
+    usize iov_len;
 };
 
 isize sys_read(unsigned int fd, void* buf, usize size);
@@ -30,15 +49,28 @@ isize sys_write(unsigned int fd, const void* buf, usize size);
 int sys_open(const char* path, u32 flags);
 int sys_close(unsigned int fd);
 isize sys_seek(int fd, isize offset, int whence);
+int mprotect(void* addr, usize size, int prot);
+u64 sys_brk(void* addr);
 int sys_ioctl(int fd, int cmd, void* arg);
+isize sys_writev(int fd, const iovec* iov, int iovcnt);
 int sys_dup(int oldfd);
 int sys_dup2(int oldfd, int newfd);
 int sys_fork();
 int sys_execve(const char* path, char* const argv[], char* const envp[]);
 void sys_exit(int status);
 int sys_wait(int pid, int* status, int options);
+int sys_uname(void* buf);
+int sys_fcntl(int fd, int op, u64 arg);
 int sys_getdents(int fd, void* buf, usize size);
 int sys_getcwd(char* buf, usize size);
 int sys_chdir(const char* path);
+int sys_unlink(const char* path);
+isize sys_readlink(const char* path, char* buf, usize size);
+int sys_getuid();
+int sys_getgid();
+int sys_geteuid();
+int sys_getegid();
+int sys_arch_prctl(int op, u64* addr);
+int sys_openat(int dirfd, const char* path, u32 flags);
 
 int sys_debug(const char* str);
--- src/syscalls.cpp
+++ src/syscalls.cpp
@@ -1,6 +1,6 @@
 #include <syscalls.h>
 
-int do_syscall(CPU* cpu, int num)
+u64 do_syscall(CPU* cpu, int num)
 {
     switch (num)
     {
@@ -9,16 +9,29 @@ int do_syscall(CPU* cpu, int num)
     case SYS_OPEN:          return sys_open((const char*)cpu->rdi, cpu->rsi);
     case SYS_CLOSE:         return sys_close(cpu->rdi);
     case SYS_SEEK:          return sys_seek(cpu->rdi, cpu->rsi, cpu->rdx);
+    case SYS_MPROTECT:      return mprotect((void*)cpu->rdi, cpu->rsi, cpu->rdx);
+    case SYS_BRK:           return sys_brk((void*)cpu->rdi);
     case SYS_IOCTL:         return sys_ioctl(cpu->rdi, cpu->rsi, (void*)cpu->rdx);
+    case SYS_WRITEV:        return sys_writev(cpu->rdi, (const iovec*)cpu->rsi, cpu->rdx);
     case SYS_DUP:           return sys_dup(cpu->rdi);
     case SYS_DUP2:          return sys_dup2(cpu->rdi, cpu->rsi);
     case SYS_FORK:          return sys_fork();
     case SYS_EXECVE:        return sys_execve((const char*)cpu->rdi, (char* const*)cpu->rsi, (char* const*)cpu->rdx);
     case SYS_EXIT:          sys_exit(cpu->rdi); return 0;
     case SYS_WAIT:          return sys_wait(cpu->rdi, (int*)cpu->rsi, cpu->rdx);
+    case SYS_UNAME:         return sys_uname((void*)cpu->rdi);
+    case SYS_FCNTL:         return sys_fcntl(cpu->rdi, cpu->rsi, cpu->rdx);
     case SYS_GETDENTS:      return sys_getdents(cpu->rdi, (char*)cpu->rsi, cpu->rdx);
     case SYS_GETCWD:        return sys_getcwd((char*)cpu->rdi, cpu->rsi);
     case SYS_CHDIR:         return sys_chdir((const char*)cpu->rdi);
+    case SYS_UNLINK:        return sys_unlink((const char*)cpu->rdi);
+    case SYS_READLINK:      return sys_readlink((const char*)cpu->rdi, (char*)cpu->rsi, cpu->rdx);
+    case SYS_GETUID:        return sys_getuid();
+    case SYS_GETGID:        return sys_getgid();
+    case SYS_GETEUID:       return sys_geteuid();
+    case SYS_GETEGID:       return sys_getegid();
+    case SYS_ARCH_PRCTL:    return sys_arch_prctl(cpu->rdi, (u64*)cpu->rsi);
+    case SYS_OPENAT:        return sys_openat(cpu->rdi, (const char*)cpu->rsi, cpu->rdx);
     case SYS_DEBUG:         return sys_debug((const char*)cpu->rdi);
     default:                return -ERR_NOT_IMPL;
     }
@@ -27,6 +40,11 @@ int do_syscall(CPU* cpu, int num)
 extern "C" void syscall_handler(CPU* cpu, int num)
 {
     cpu->rax = do_syscall(cpu, num);
+
+    if ((ErrorCode)cpu->rax == -ERR_NOT_IMPL)
+        kprintf(WARN "syscall %d not implemented\n", num);
+    // else
+    //     kprintf(INFO "syscall %d returned %lx\n", num, cpu->rax);
 }
 
 isize sys_read(unsigned int fd, void* buf, usize size)
@@ -92,6 +110,39 @@ isize sys_seek(int fd, isize offset, int whence)
     return file->seek(offset, whence);
 }
 
+int mprotect(void* addr, usize size, int prot)
+{
+    return 0;
+}
+
+u64 sys_brk(void* addr)
+{
+    u64 min_brk = running->mm->start_brk;
+    u64 org_brk = running->mm->brk;
+    u64 new_brk = (u64)addr;
+
+    if (new_brk < min_brk)
+        return org_brk;
+
+    u64 org_brk_page = PAGE_ALIGN(org_brk);
+    u64 new_brk_page = PAGE_ALIGN(new_brk);
+
+    if (new_brk_page > org_brk_page)
+    {
+        usize pages = PAGE_COUNT(new_brk_page - org_brk_page);
+
+        vmm.alloc_pages(running->mm->pml4, org_brk_page, pages, PE_WRITE | PE_USER);
+    }
+    else if (new_brk_page < org_brk_page)
+    {
+        // free pages
+    }
+
+    running->mm->brk = new_brk;
+
+    return new_brk;
+}
+
 int sys_ioctl(int fd, int cmd, void* arg)
 {
     auto file = running->fdt.get(fd);
@@ -100,6 +151,28 @@ int sys_ioctl(int fd, int cmd, void* arg)
         return file.error();
 
     return file->ioctl(cmd, arg);
+}
+
+isize sys_writev(int fd, const iovec* iov, int iovcnt)
+{
+    auto file = running->fdt.get(fd);
+
+    if (!file)
+        return file.error();
+
+    isize total = 0;
+
+    for (int i = 0; i < iovcnt; i++)
+    {
+        isize ret = file->write(iov[i].iov_base, iov[i].iov_len);
+
+        if (ret < 0)
+            return ret;
+
+        total += ret;
+    }
+
+    return total;
 }
 
 int sys_dup(int oldfd)
@@ -171,6 +244,33 @@ int sys_wait(int pid, int* status, int options)
     return 0;
 }
 
+int sys_uname(void* buf)
+{
+    struct utsname
+    {
+        char sysname[65];
+        char nodename[65];
+        char release[65];
+        char version[65];
+        char machine[65];
+    };
+
+    auto uname = (utsname*)buf;
+
+    strcpy(uname->sysname, "Linux");
+    strcpy(uname->nodename, "localhost");
+    strcpy(uname->release, "5.12.167");
+    strcpy(uname->version, "5.12.167");
+    strcpy(uname->machine, "x86_64");
+
+    return 0;
+}
+
+int sys_fcntl(int fd, int op, u64 arg)
+{
+    return 1;
+}
+
 int sys_getdents(int fd, void* buf, usize size)
 {
     auto file = running->fdt.get(fd);
@@ -228,6 +328,58 @@ int sys_chdir(const char* path)
     running->cwd = inode.ptr;
 
     return 0;
+}
+
+int sys_unlink(const char* path)
+{
+    return 0;
+}
+
+isize sys_readlink(const char* path, char* buf, usize size)
+{
+    return -1;
+}
+
+int sys_getuid()
+{
+    return 0;
+}
+
+int sys_getgid()
+{
+    return 0;
+}
+
+int sys_geteuid()
+{
+    return 0;
+}
+
+int sys_getegid()
+{
+    return 0;
+}
+
+int sys_arch_prctl(int op, u64* addr)
+{
+    // ARCH_SET_FS
+    if (op == 0x1002)
+    {
+        wrmsr(0xc0000100, (u64)addr);
+        return 0;
+    }
+
+    return -1;
+}
+
+#define AT_FDCWD    (-100)
+
+int sys_openat(int dirfd, const char* path, u32 flags)
+{
+    if (dirfd != AT_FDCWD)
+        return -ERR_BAD_FD;
+
+    return sys_open(path, flags);
 }
 
 int sys_debug(const char* str)
