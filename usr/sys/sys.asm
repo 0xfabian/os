51163a96e7d6f8e838e76d99af059d2a05f46bc9
0xfabian <dumitru_fabian0@yahoo.com>
2025-04-02 23:04:26 +0300

    macros for syscalls

--- usr/sys/sys.asm
+++ usr/sys/sys.asm
@@ -1,102 +1,42 @@
 [bits 64]
 
-global open
-global close
-global write
-global seek
-global ioctl
-global read
-global dup
-global dup2
-global fork
-global execve
-global wait4
-global exit
-global getcwd
-global chdir
-global getdents
-global debug
+%macro SYSCALL 2
 
-open:
-    mov eax, 2
+global %1
+%1:
+    mov eax, %2
     syscall
     ret
 
-close:
-    mov eax, 3
+%endmacro
+
+%macro SYSCALL4 2
+
+global %1
+%1:
+    mov r10, rcx    ; rcx gets clobbered on syscall
+    mov eax, %2     ; so the 4th argument is passed in r10
     syscall
     ret
 
-read:
-    mov eax, 0
-    syscall
-    ret
+%endmacro
 
-write:
-    mov eax, 1
-    syscall
-    ret
-
-seek:
-    mov eax, 8
-    syscall
-    ret
-
-ioctl:
-    mov eax, 16
-    syscall
-    ret
-
-dup:
-    mov eax, 32
-    syscall
-    ret
-
-dup2:
-    mov eax, 33
-    syscall
-    ret
-
-fork:
-    mov eax, 57
-    syscall
-    ret
-
-execve:
-    mov eax, 59
-    syscall
-    ret
-
-wait4:
-    mov r10, rcx ; rcx gets clobbered on syscall
-    mov eax, 61  ; so the 4th argument is passed in r10
-    syscall
-    ret
-
-exit:
-    mov eax, 60
-    syscall
-    ret
-
-getcwd:
-    mov eax, 79
-    syscall
-    ret
-
-chdir:
-    mov eax, 80
-    syscall
-    ret
-
-getdents:
-    mov eax, 78
-    syscall
-    ret
-
-debug:
-    mov eax, 512
-    syscall
-    ret
+SYSCALL     open,       2
+SYSCALL     close,      3
+SYSCALL     read,       0
+SYSCALL     write,      1
+SYSCALL     seek,       8
+SYSCALL     ioctl,      16
+SYSCALL     dup,        32
+SYSCALL     dup2,       33
+SYSCALL     fork,       57
+SYSCALL     execve,     59
+SYSCALL4    wait4,      61
+SYSCALL     exit,       60
+SYSCALL     getcwd,     79
+SYSCALL     chdir,      80
+SYSCALL     getdents,   78
+SYSCALL     debug,      512
 
 global _start
 extern main
